2e1c5f6a4f6cab2588d4a906429312fe9b49a5ed
williamsg <williamsg@163.com>
2018-05-04 11:54:01 +0800

    Fix dependency sequence error. Add test and package dependency, use local relative path temporary

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,4 +1,4 @@
 const gentypes = require('./gentypes.json')
 const basetypes = require('./basetypes.json')
 
-module.exports = Object.assign({}, gentypes, basetypes)
+module.exports = Object.assign({}, basetypes, gentypes)
